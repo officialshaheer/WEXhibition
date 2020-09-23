88031666c2128e88d6d6dc9c3c36d24baef96d0c
Shaheer <officialshaheer@gmail.com>
2020-09-24 01:23:35 +0530

    Initialised the project with the basic files
    main.html
    main.js
    main.css

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var container, scene, camera, renderer;
+
+var controls;
+
+init();
+animate();
+
+function init() {
+	container = document.getElementById('container');
+ 
+}
+
+function animate() {
+
+}
+
+function render() {
+	
+}
