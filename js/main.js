09a02e0ee8b7b051f3da4b4bf11754422f6dab9b
Shaheer <officialshaheer@gmail.com>
2020-09-24 01:37:23 +0530

    Update main.js
    Basic Setup and Initialisation main.js functions

--- js/main.js
+++ js/main.js
@@ -6,14 +6,41 @@ init();
 animate();
 
 function init() {
+
+	// Basic Setup
 	container = document.getElementById('container');
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight,1, 1000 );
+
+	renderer = new THREE.WebGLRenderer( {alpha: true} );
+	renderer.setSize( window.innerWidth, window.innerHeight);
+
+	// Events
+	window.addEventListener("resize", onWindowResize, false);
+
+	container.appendChild(renderer.domElement);
+	document.body.appendChild( container );
  
 }
 
 function animate() {
 
+	requestAnimationFrame( animate );
+	render();
 }
 
 function render() {
+
+	renderer.clear();
+	renderer.render(scene,camera);
 	
+}
+
+function onWindowResize() {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight);
 }
